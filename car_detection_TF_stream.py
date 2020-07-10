ecee420b10620cc82211a59213e5e17168d971c0
Allen Zhang <AllenZhang@allenzhthegreat.lan>
2020-07-10 16:06:08 -0500

    Video Stream Added

---
+++ car_detection_TF_stream.py
@@ -0,0 +1,126 @@
+import picamera
+from picamera.array import PiRGBArray 
+import time
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+import cv2
+
+class VideoStream:
+    """Camera object that controls video streaming from the Picamera"""
+    def __init__(self, resolution=(640, 480), framerate=32):
+        # Initialize PiCamera and the camera stream
+        self.stream = cv2.VideoCapture(0)
+        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+        ret = self.stream.set(3,resolution[0])
+        ret = self.stream.set(4,resolution[1])
+            
+        # Read first frame from the stream
+        (self.grabbed, self.frame) = self.stream.read()
+
+	# Variable to control when the camera is stopped
+        self.stopped = False
+
+    def start(self):
+	# Start the thread that reads frames from the video stream
+        Thread(target=self.update,args=()).start()
+        return self
+
+    def update(self):
+        # Keep looping indefinitely until the thread is stopped
+        while True:
+            # If the camera is stopped, stop the thread
+            if self.stopped:
+                # Close camera resources
+                self.stream.release()
+                return
+
+            # Otherwise, grab the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+	# Return the most recent frame
+        return self.frame
+
+    def stop(self):
+	# Indicate that the camera and thread should be stopped
+        self.stopped = True
+
+
+# initialize parameters
+min_conf_threshold = 0.5    # 0.35 seems to work well with traffic pic
+imH = 320
+imW = 320
+PATH_TO_LABELS = 'labelmap.txt'
+#image_path = 'test_image3.jpg'
+
+# Load the label map
+with open(PATH_TO_LABELS, 'r') as f:
+    labels = [line.strip() for line in f.readlines()]
+
+# Load the TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+input_shape = input_details[0]['shape']
+
+# Initialize video stream
+videostream = VideoStream(resolution=(imW,imH),framerate=32).start()
+time.sleep(1)
+
+while True:
+
+    # Start timer (for calculating frame rate)
+    t1 = cv2.getTickCount()
+
+    # Get the most recent frame
+    frame_cur = videostream.read()
+    frame = frame_cur.copy()
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_resized = cv2.resize(frame_rgb, (input_shape[0], input_shape[1]))
+    input_data = np.expand_dims(frame_resized, axis=0)
+
+    # Perform the actual detection by running the model with the image as input
+    interpreter.set_tensor(input_details[0]['index'],input_data)
+    interpreter.invoke()
+    
+    # The function `get_tensor()` returns a copy of the tensor data.
+    # Use `tensor()` in order to get a pointer to the tensor.
+    boxes = interpreter.get_tensor(output_details[0]['index'])[0]
+    classes = interpreter.get_tensor(output_details[1]['index'])[0]
+    classes = classes + 1
+    scores = interpreter.get_tensor(output_details[2]['index'])[0]
+    num = interpreter.get_tensor(output_details[3]['index'])[0]
+
+    for i in range(len(scores)):
+    if (scores[i] > min_conf_threshold):
+
+        # Get bounding box coordinates and draw box
+        # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+        ymin = int(max(1,(boxes[i][0] * imH)))
+        xmin = int(max(1,(boxes[i][1] * imW)))
+        ymax = int(min(imH,(boxes[i][2] * imH)))
+        xmax = int(min(imW,(boxes[i][3] * imW)))
+        
+        cv2.rectangle(image_cv, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+
+        # Draw label
+        object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
+        label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
+        labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.3, 1) # Get font size
+        label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+        cv2.rectangle(image_cv, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
+        cv2.putText(image_cv, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 0), 1) # Draw label text
+
+    cv2.imshow('Object detector', frame)
+
+    # Press 'q' to quit
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Clean up
+videostream.stop()
