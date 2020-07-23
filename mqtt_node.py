8bb2c0e4f178f5ad9e83637e6f5110f4ab797d79
Jake Pu <pujianhao@126.com>
2020-07-23 16:22:29 -0500

    mqtt logic finished & untested

--- mqtt_node.py
+++ mqtt_node.py
@@ -1,31 +1,42 @@
 import paho.mqtt.client as mqtt
 import socket # socket.gethostname()
 import re
+client = mqtt.Client()
+traffic = 5
+is_go = False
+payload = ""
+hostname = socket.gethostname()
+machine_number = re.search(r'\d+', hostname).group()
+def current_station():
+    global is_go, payload
+    if int(payload) == machine_number:
+        is_go = True
+    else:
+        is_go = False
+def request_received():
+    global payload, hostname
+    payload = hostname + ": Traffic - " + str(traffic)
+    client.publish("Traffic", payload = payload)
+switcher = {"Current_Station": current_station, "Request": request_received}
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("Traffic/#")
     client.subscribe("Current_Station")
-
+    client.subscribe("Request")
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    if msg.topic == "Current_Station":
-        if msg.payload == machine_number:
-            is_go = True
-        else:
-            is_go = False
-
-client = mqtt.Client()
+    global payload, switcher
+    payload = msg.payload
+    switcher[msg.topic]
 client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect("stc1")
 hostname = socket.gethostname()
 machine_number = re.findall(r'\d+', hostname)[0]
-is_go = False
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
--- mqtt_server.py
+++ mqtt_server.py
@@ -2,32 +2,80 @@
 
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
-
+import threading
+from time import sleep
+import re
+import socket # socket.gethostname()
+hostname = socket.gethostname()
+machine_number = re.search(r'\d+', hostname).group()
 NUM_STATIONS = 2
 count = NUM_STATIONS
-dict_stations = {}
+stations_dict = {}
+is_go = False
+payload = ""
+traffic = 0
+wait_dict = {}
+online_stations = []
+def current_station():
+    global is_go, machine_number
+    if payload == machine_number:
+        is_go = True
+    else:
+        is_go = False
+def read_message():
+    global stations_dict, payload 
+    hostname, _, traffic = payload.partition(': Traffic - ')
+    try:
+        stations_dict[hostname] = int(traffic)
+        online_stations.append(hostname)
+        #wait_dict[hostname] = 1+wait_dict[hostname] if hostname in wait_dict else 1
+        if hostname not in wait_dict:
+            wait_dict[hostname] = 0
+    except:
+        pass
 
+switcher = {"Traffic": read_message, "Current_Station": current_station}
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("Traffic/#")
+    client.subscribe("Traffic")
     client.subscribe("Current_Station")
-    client.subscribe("Station_Information")
-
+    client.subscribe("Request")
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    global count 
-    dict_stations[msg.topic] = msg.payload
-    count = count - 1
-    if count == 0:
-        sorted_dict = sorted(dict_stations.items(), key=lambda x: x[1], reverse = True)
-        client.publish("Current_Station", sorted_dict[0][0], hostname="stc1")
-        count = NUM_STATIONS
-        dict_stations.clear()
+    global payload
+    payload = msg.payload
+    switcher[msg.topic]
 
+def request():
+    stations_dict.clear()
+    online_stations.clear()
+    client.publish("Request", 1)
+def status_update():
+    global hostname,traffic
+    request()
+    sleep(15) # wait for other stations to feedback their traffic
+    stations_dict[hostname] = int(traffic) # input the server's own traffic
+    choose_current_station()
+def choose_current_station():
+    current_station = 'stc0'
+    longest_wait = 0
+    for station in online_stations:
+        if wait_dict[station] >= 3:
+            current_station = station
+            longest_wait = wait_dict[station]
+    if longest_wait > 0:
+        wait_dict[station] = 0
+        return current_station
+    rank = sorted(stations_dict.items(), key= lambda item: item[1], reverse=True)
+    for i in range(len(rank)):
+        if rank[i][0] in online_stations:
+            current_station = rank[i][0]
+            break
+    return current_station
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -35,12 +83,11 @@ client.on_message = on_message
 
 client.connect("stc1", 1883, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
+# start a thread to handle network traffic
 client.loop_start()
 
 # Unresovled issues:
 # how to read information from this device and add it to the dict
 # how to request other node devices for information
+
+threading.Timer(60, status_update)
